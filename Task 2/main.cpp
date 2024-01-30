988f3fba8ac5f938f042bbca43e40b9ba02d16f2
Thomas Smale <67895142+alphasoyboi@users.noreply.github.com>
2024-01-30 16:03:20 +0000

    started work on task 2
    - added trackbars to select object tracking color
    - added filtered output window

--- Task 2/main.cpp
+++ Task 2/main.cpp
@@ -11,50 +11,87 @@
 using namespace std;
 using namespace cv;
 
+static const String kWinTitleRaw      = "left";
+static const String kWinTitleFiltered = "left filtered";
+
+static const int kMaxH = 360;
+static const int kMaxSV = 1000;
+static int lowHue = 0, lowSat = 0, lowVal = 0;
+static int highHue = kMaxH, highSat = kMaxSV, highVal = kMaxSV;
+
+static void onLowHueThreshTrackbar(int, void *);
+static void onHighHueThreshTrackbar(int, void *);
+static void onLowSatThreshTrackbar(int, void *);
+static void onHighSatThreshTrackbar(int, void *);
+static void onLowValThreshTrackbar(int, void *);
+static void onHighValThreshTrackbar(int, void *);
+
 int main()
 {
-
     //connect with the owl and load calibration values
     robotOwl owl(1500, 1475, 1520, 1525, 1520);
 
-    while (true){
+    namedWindow(kWinTitleRaw);
+    namedWindow(kWinTitleFiltered);
+
+    createTrackbar("Low H",  kWinTitleRaw, &lowHue, kMaxH, onLowHueThreshTrackbar);
+    createTrackbar("High H", kWinTitleRaw, &highHue, kMaxH, onHighHueThreshTrackbar);
+    createTrackbar("Low S",  kWinTitleRaw, &lowSat, kMaxSV, onLowSatThreshTrackbar);
+    createTrackbar("High S", kWinTitleRaw, &highSat, kMaxSV, onHighSatThreshTrackbar);
+    createTrackbar("Low V",  kWinTitleRaw, &lowVal, kMaxSV, onLowValThreshTrackbar);
+    createTrackbar("High V", kWinTitleRaw, &highVal, kMaxSV, onHighValThreshTrackbar);
+
+    bool running = true;
+    while (running){
         //read the owls camera frames
-        Mat left, right;
+        Mat left, right, hsvLeft, filteredLeft;
         owl.getCameraFrames(left, right);
 
         //your tracking code here
-
-
-
-
-
-
-
-
-
-
-
+        //left *= 1./255;
+        cvtColor(left, hsvLeft, COLOR_BGR2HSV);
+        inRange(hsvLeft, Scalar(lowHue, lowSat, lowVal), Scalar(highHue, highSat, highVal), filteredLeft);
 
         //display camera frame
-        imshow("left",left);
-        waitKey(10);
+        imshow(kWinTitleRaw, left);
+        imshow(kWinTitleFiltered, filteredLeft);
+        //imshow("left hsv", )
+        switch(waitKey(10)) {
+            case 'q':
+            case 27:
+                running = false;
+                break;
+        }
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+static void onLowHueThreshTrackbar(int, void *)
+{
+    lowHue = min(highHue-1, lowHue);
+    setTrackbarPos("Low H", kWinTitleRaw, lowHue);
+}
+static void onHighHueThreshTrackbar(int, void *)
+{
+    highHue = max(highHue, lowHue+1);
+    setTrackbarPos("High H", kWinTitleRaw, highHue);
+}
+static void onLowSatThreshTrackbar(int, void *)
+{
+    lowSat = min(highSat-1, lowSat);
+    setTrackbarPos("Low S", kWinTitleRaw, lowSat);
+}
+static void onHighSatThreshTrackbar(int, void *)
+{
+    highSat = max(highSat, lowSat+1);
+    setTrackbarPos("High S", kWinTitleRaw, highSat);
+}
+static void onLowValThreshTrackbar(int, void *)
+{
+    lowVal = min(highVal-1, lowVal);
+    setTrackbarPos("Low V", kWinTitleRaw, lowVal);
+}
+static void onHighValThreshTrackbar(int, void *)
+{
+    highVal = max(highVal, lowVal+1);
+    setTrackbarPos("High V", kWinTitleRaw, highVal);
+}
