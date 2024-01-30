105e1bb874adc5e4a80857b82a72eeafeb953b4f
Thomas Smale <67895142+alphasoyboi@users.noreply.github.com>
2024-01-30 13:42:24 +0000

    finished first revision of task 1

--- Task 1/main.cpp
+++ Task 1/main.cpp
@@ -11,12 +11,51 @@
 using namespace std;
 using namespace cv;
 
+Vec3f RGBtoHSV(const Vec3b& rgb)
+{
+    Mat3f rgbMat(static_cast<Vec3f>(rgb));
+
+    rgbMat *= 1.0/255.0;
+
+    Mat3f hsvMat;
+    cvtColor(rgbMat, hsvMat, COLOR_BGR2HSV);
+
+    Vec3f hsv = hsvMat(0,0);
+
+    return hsv;
+}
+
+string getColorString(const Vec3f& hsv)
+{
+    // this is fairly naive, but works well for the categorized colors, even if slightly desaturated.
+    // make sure color isn't black by checking value (hsv[2]),
+    //  then compare upper and lower bounds of hue (hsv[0]), and finally saturation (hsv[1]).
+    if (hsv[2] >= 0.2f) {
+        if ((hsv[0] <= 20.0f || hsv[0] >= 340.0f) && hsv[1] >= 0.5f) {// && hsv[2] >= 0.5f) {
+            return "red";
+        }
+        if ((hsv[0] <= 55.0f && hsv[0] >= 35.0f) && hsv[1] >= 0.45f) {// && hsv[2] >= 0.45f) {
+            return "yellow";
+        }
+        if ((hsv[0] <= 175.0f && hsv[0] >= 100.0f) && hsv[1] >= 0.3f) {// && hsv[2] >= 0.3f) {
+           return "green";
+        }
+        if ((hsv[0] <= 245.0f && hsv[0] >= 185.0f) && hsv[1] >= 0.35f) {// && hsv[2] >= 0.5f) {
+           return "blue";
+        }
+        if ((hsv[0] <= 335.0f && hsv[0] >= 295.0f) && hsv[1] >= 0.3f) {// && hsv[2] >= 0.45f) {
+           return "magenta";
+        }
+    }
+    return "n/a";
+}
+
 int main()
 {
     //connect with the owl and load calibration values
     robotOwl owl(1500, 1475, 1520, 1525, 1520, true); //starts in "quiet mode" which switches off the servos.
 
-    while (true){
+    while (true) {
         //read the owls camera frames
         Mat left, right;
         owl.getCameraFrames(left, right);
@@ -24,27 +63,20 @@ int main()
         //get pixel colour values
         Point centrePoint(left.size().width/2, left.size().height/2);
         Vec3b pixelValue = left.at<Vec3b>(centrePoint);
-        unsigned char red  =pixelValue[2];
-        unsigned char green=pixelValue[1];
-        unsigned char blue =pixelValue[0];
 
         //drawing functions
         circle(left, centrePoint, 15, Scalar(255,255,255), 2); //draw a circle to show the pixel being measured
-        string text = "(" + to_string(red) + ", " + to_string(green) + ", " + to_string(blue) + ")";      //create a string of the RGB values
-        putText(left, text, centrePoint+Point(-100,50), FONT_HERSHEY_SIMPLEX, 1, Scalar(255,255,255), 2); //draw the string to the image
+        Vec3f hsv = RGBtoHSV(pixelValue); // convert the pixel to hsv
+        string hsvText = "(" + to_string(hsv[0]) + ", " + to_string(hsv[1]) + ", " + to_string(hsv[2]) + ")";
+        putText(left, hsvText, centrePoint+Point(-250,100), FONT_HERSHEY_SIMPLEX, 1, Scalar(255,255,255), 2); //draw the string containing hsv components to the image
+        string colorText = getColorString(hsv);
+        putText(left, colorText, centrePoint+Point(-50,50), FONT_HERSHEY_SIMPLEX, 1, Scalar(255,255,255), 2); //draw the string denoting the estimated color to the image
 
         //display image
-        imshow("left",left);
+        imshow("left", left);
         waitKey(30);
     }
 }
-
-
-
-
-
-
-
 
 
 
