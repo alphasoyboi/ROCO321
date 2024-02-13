dad8e344883fc4a2261f056eda09a1d6a62abe59
Thomas Smale <67895142+alphasoyboi@users.noreply.github.com>
2024-02-13 16:48:59 +0000

    task 3: added basic tracking for left eye

--- Task 3/main.cpp
+++ Task 3/main.cpp
@@ -20,28 +20,32 @@ int main()
 
     //===========================target selection loop==============================
     Mat target;
-    Rect targetPos(320-targetSize/2, 240-targetSize/2, targetSize, targetSize);
+    Rect targetPos(320 - targetSize/2, 240 - targetSize/2, targetSize, targetSize);
     while (true){
         //read the owls camera frames
         Mat left, right;
         owl.getCameraFrames(left, right);
 
         //if SPACE is pressed, save the target image and break the loop
-        if(waitKey(10)==' ')
+        if(waitKey(10) == ' ')
         {
             left(targetPos).copyTo(target);
-            cout<<"Target selected!"<<endl;
+            cout << "Target selected!" << endl;
             break;
         }
 
         //display camera frame
-        rectangle(left, targetPos, Scalar(255,255,255), 2);
-        imshow("left",left);        
+        rectangle(left, targetPos, Scalar(255, 255, 255), 2);
+        imshow("left", left);
     }
 
     //===========================target tracking loop================================
-    cout<<"Starting tracking code..."<<endl;
-    while(1)
+    cout << "Starting tracking code..." << endl;
+    Mat match;
+    double minVal, maxVal;
+    Point minLoc, maxLoc;
+    Rect topLeft;
+    while (true)
     {
         //read the owls camera frames
         Mat left, right;
@@ -49,10 +53,10 @@ int main()
 
         //your tracking code goes here
 
+        matchTemplate(left, target, match, TM_SQDIFF_NORMED);
+        minMaxLoc(match, &minVal, &maxVal, &minLoc, &maxLoc);
 
-
-
-
+        rectangle(left, {minLoc.x, minLoc.y, targetSize, targetSize}, 255, 2);
 
 
 
@@ -68,10 +72,9 @@ int main()
         imshow("left", left);
         imshow("right", right);
         imshow("target", target);
+        imshow("match", match);
         waitKey(10);
     }
-
-
 }
 
 
