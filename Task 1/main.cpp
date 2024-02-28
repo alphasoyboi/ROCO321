8534289a92a5beb5eaf640cf161afdd3ef07e872
Thomas Smale <67895142+alphasoyboi@users.noreply.github.com>
2024-02-28 16:31:31 +0000

    a couple names

--- Task 1/main.cpp
+++ Task 1/main.cpp
@@ -11,17 +11,17 @@
 using namespace std;
 using namespace cv;
 
-Vec3f RGBtoHSV(const Vec3b& rgb)
+Vec3f BGRtoHSV(const Vec3b& rgb)
 {
     // create matrix from rgb pixel
-    Mat3f rgbMat(static_cast<Vec3f>(rgb));
+    Mat3f bgrMat(static_cast<Vec3f>(rgb));
 
-    // normalize values
-    rgbMat *= 1.0/255.0;
+    // ensure values are normalized
+    bgrMat *= 1.0/255.0;
 
     // convert image to hsv color space
     Mat3f hsvMat;
-    cvtColor(rgbMat, hsvMat, COLOR_BGR2HSV);
+    cvtColor(bgrMat, hsvMat, COLOR_BGR2HSV);
 
     // pull out pixel value to return
     Vec3f hsv = hsvMat(0,0);
@@ -32,7 +32,7 @@ string getColorString(const Vec3f& hsv)
 {
     // this is fairly naive, but works well for the categorized colors, even if slightly desaturated.
     // make sure color isn't black by checking value (hsv[2]),
-    //  then compare upper and lower bounds of hue (hsv[0]), and finally saturation (hsv[1]).
+    // then compare upper and lower bounds of hue (hsv[0]), and finally saturation (hsv[1]).
     if (hsv[2] >= 0.2f) {
         if ((hsv[0] <= 20.0f || hsv[0] >= 340.0f) && hsv[1] >= 0.5f) {// && hsv[2] >= 0.5f) {
             return "red";
@@ -59,23 +59,23 @@ int main()
     robotOwl owl(1500, 1475, 1520, 1525, 1520, true); //starts in "quiet mode" which switches off the servos.
 
     while (true) {
-        //read the owls camera frames
+        // read the owls camera frames
         Mat left, right;
         owl.getCameraFrames(left, right);
 
-        //get pixel colour values
+        // get pixel colour values
         Point centrePoint(left.size().width/2, left.size().height/2);
         Vec3b pixelValue = left.at<Vec3b>(centrePoint);
 
-        //drawing functions
-        circle(left, centrePoint, 15, Scalar(255,255,255), 2); //draw a circle to show the pixel being measured
-        Vec3f hsv = RGBtoHSV(pixelValue); // convert the pixel to hsv
+        // drawing functions
+        circle(left, centrePoint, 15, Scalar(255,255,255), 2); // draw a circle to show the pixel being processed
+        Vec3f hsv = BGRtoHSV(pixelValue); // convert the pixel to hsv
         string hsvText = "(" + to_string(hsv[0]) + ", " + to_string(hsv[1]) + ", " + to_string(hsv[2]) + ")";
         putText(left, hsvText, centrePoint+Point(-250,100), FONT_HERSHEY_SIMPLEX, 1, Scalar(255,255,255), 2); //draw the string containing hsv components to the image
         string colorText = getColorString(hsv);
         putText(left, colorText, centrePoint+Point(-50,50), FONT_HERSHEY_SIMPLEX, 1, Scalar(255,255,255), 2); //draw the string denoting the estimated color to the image
 
-        //display image
+        // display image
         imshow("left", left);
         waitKey(30);
     }
